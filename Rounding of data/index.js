287c18067d3f628459b97123e2500cd4ff03b520
misbah-aslam16 <misbahaslam000@gmail.com>
2024-06-24 11:31:30 -0700

    new file added

---
+++ Rounding of data/index.js
@@ -0,0 +1,26 @@
+//---------------Math.round---------------//
+
+var percentage = Math.round(67.78);
+console.log(percentage);
+
+
+var percentage = Math.round(75.49);
+console.log(percentage);
+
+//---------------Math.ceil---------------//
+
+var percentage = Math.ceil(59.82);
+console.log(percentage);
+
+var percentage = Math.ceil(75.00);
+console.log(percentage);
+
+//---------------Math.floor---------------//
+
+var percentage = Math.floor(85.99);
+console.log(percentage);
+
+//---------------Math.random---------------//
+
+var randomNumber = Math.random();
+console.log(randomNumber);
